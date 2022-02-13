78eb67afe12f49cd7a53b770493416b4204caf77
yusuferarsln <yusuferarslann@gmail.com>
2022-02-13 17:43:14 +0300

    bak bu commit

---
+++ lib/user_definiton/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+TextStyle kQuestionStyle =
+    TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold);
+TextStyle kAnswerStyle = TextStyle(
+    color: Colors.lightBlue, fontSize: 27, fontWeight: FontWeight.bold);
---
+++ lib/user_definiton/definiton_pageview.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:stepway/pages/starting_page.dart';
+
+class DefinitonPage extends StatefulWidget {
+  const DefinitonPage({Key? key}) : super(key: key);
+
+  @override
+  State<DefinitonPage> createState() => _DefinitonPageState();
+}
+
+class _DefinitonPageState extends State<DefinitonPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          's',
+          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Önce seni biraz tanıyalım.",
+              style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  labelText: 'Ad soyad',
+                  hintText: 'Ad soyad'),
+            ),
+            Text("Cinsiyet"),
+            Row(
+              children: [
+                Text("K"),
+                Radio(
+                  value: 0,
+                  groupValue: "cinsiyet",
+                  onChanged: (_) {},
+                ),
+                Text("E"),
+                Radio(
+                  value: 0,
+                  groupValue: "cinsiyet",
+                  onChanged: (_) {},
+                ),
+                Text("N.B."),
+                Radio(
+                  value: 0,
+                  groupValue: "cinsiyet",
+                  onChanged: (_) {},
+                ),
+              ],
+            ),
+            Text("Doğum Tarihi"),
+
+            //DATE PİCKER
+
+            Text("Avatar Seçiniz"),
+            Image.network("src"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.deepPurple),
+                  ),
+                  onPressed: () {
+                    Get.to(StartingPage());
+                  },
+                  child: Row(mainAxisSize: MainAxisSize.min, children: const [
+                    Text(
+                      "Tamamla",
+                      style: TextStyle(fontSize: 17),
+                    ),
+                    SizedBox.square(dimension: 4),
+                    Icon(Icons.arrow_forward_ios_sharp),
+                  ]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
